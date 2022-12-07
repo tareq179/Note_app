89a5ea9c3be191e96b071230b1668506ae37e19e
tareq179 <abdulkarimctg373@gmail.com>
2022-12-07 22:36:38 +0600

    Live Search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,25 +1,46 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
-import colors from '../theme/colors'
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React from "react";
+import colors from "../theme/colors";
+import { AntDesign } from "@expo/vector-icons";
 
-const SearchBar = ({containerStyle}) => {
+const SearchBar = ({ containerStyle, value, onClear, onChangeText }) => {
   return (
-    <View style={[styles.container,{...containerStyle}]}>
-      <TextInput style={styles.searchBar} placeholder='Search hear...'/>
+    <View style={[styles.container, { ...containerStyle }]}>
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        style={styles.searchBar}
+        placeholder="Search hear..."
+      />
+      {value ? (
+        <AntDesign
+          name="close"
+          size={20}
+          color={colors.PRIMARY}
+          onPress={onClear}
+          style={styles.clearIcon}
+        />
+      ) : null}
     </View>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
 
 const styles = StyleSheet.create({
-    searchBar:{
-        borderWidth:0.5,
-        borderColor:colors.PRIMARY,
-        height:40,
-        borderRadius:40,
-        paddingLeft:15,
-        fontSize:20
-    },
-    container:{}
-})
+  searchBar: {
+    borderWidth: 0.5,
+    borderColor: colors.PRIMARY,
+    height: 40,
+    borderRadius: 40,
+    paddingLeft: 15,
+    fontSize: 20,
+  },
+  clearIcon: {
+    position: "absolute",
+    right: 10,
+  },
+  container: {
+    justifyContent:'center'
+  },
+});
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -15,12 +15,14 @@ import NoteInputModal from "../components/NoteInputModal";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Note from "../components/Note";
 import { useNotes } from "../Contexts/NoteProvider";
+import NotFound from "../components/NotFound";
 
 const Home = ({ user, navigation }) => {
   const [greet, setGreet] = useState("");
   const [modalVisiable, setModalVisiable] = useState(false);
-
-  const { notes, setNotes } = useNotes();
+  const [searchQuery, setSearchQuery] = useState("");
+  const { notes, setNotes, findNotes } = useNotes();
+  const [resultNotFound, setResultNotFound] = useState(false);
 
   const findGreet = () => {
     const hrs = new Date().getHours();
@@ -28,7 +30,6 @@ const Home = ({ user, navigation }) => {
     if (hrs === 1 || hrs < 17) return setGreet("Afternoon");
     setGreet("Evening");
   };
-
 
   useEffect(() => {
     findGreet();
@@ -41,30 +42,67 @@ const Home = ({ user, navigation }) => {
     await AsyncStorage.setItem("notes", JSON.stringify(updatedNotes));
   };
 
+  const handleOnSearchInput = async (text) => {
+    setSearchQuery(text);
+    if (!text.trim()) {
+      setSearchQuery("");
+      setResultNotFound(false);
+      return await findNotes();
+    }
+    const filteredNotes = notes.filter((note) => {
+      if (note.title.toLowerCase().includes(text.toLowerCase())) {
+        return note;
+      }
+    });
+
+    if (filteredNotes.length) {
+      setNotes([...filteredNotes]);
+    } else {
+      setResultNotFound(true);
+    }
+  };
+
+  const handleOnClear = async () => {
+    setSearchQuery("");
+    setResultNotFound(false);
+    await findNotes();
+  };
+
   const openNote = (note) => {
-    navigation.navigate('NoteDetails', {note})
-  }
+    navigation.navigate("NoteDetails", { note });
+  };
 
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor={colors.LIGHT} />
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      {/* <TouchableWithoutFeedback onPress={Keyboard.dismiss}> */}
 
       <View style={styles.container}>
         <Text style={styles.header}>{`Good ${greet} ${user.name}`}</Text>
         {notes.length ? (
-          <SearchBar containerStyle={{ marginVertical: 15 }} />
+          <SearchBar
+            value={searchQuery}
+            onChangeText={handleOnSearchInput}
+            onClear={handleOnClear}
+            containerStyle={{ marginVertical: 15 }}
+          />
         ) : null}
-        <FlatList
-          numColumns={2}
-          columnWrapperStyle={{
-            justifyContent: "space-between",
-            marginBottom: 15,
-          }}
-          data={notes}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => <Note onPress={() =>openNote(item)} item={item} />}
-        />
+        {resultNotFound ? (
+          <NotFound />
+        ) : (
+          <FlatList
+            numColumns={2}
+            columnWrapperStyle={{
+              justifyContent: "space-between",
+              marginBottom: 15,
+            }}
+            data={notes}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <Note onPress={() => openNote(item)} item={item} />
+            )}
+          />
+        )}
         {!notes.length ? (
           <View
             style={[
@@ -81,7 +119,7 @@ const Home = ({ user, navigation }) => {
           style={styles.addbtn}
         />
       </View>
-      </TouchableWithoutFeedback>
+      {/* </TouchableWithoutFeedback> */}
       <NoteInputModal
         visible={modalVisiable}
         onClose={() => setModalVisiable(false)}
@@ -119,6 +157,6 @@ const styles = StyleSheet.create({
     position: "absolute",
     right: 30,
     bottom: 40,
-    zIndex:1
+    zIndex: 1,
   },
 });
